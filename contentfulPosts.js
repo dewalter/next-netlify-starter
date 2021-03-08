7d33ec10550c69efd7f500699503e541cbb243fe
Walter <wkrijt@gmail.com>
2021-03-08 15:39:03 +0100

    Create contentfulPosts.js

---
+++ contentfulPosts.js
@@ -0,0 +1,15 @@
+const space = process.env.Space_ID
+const accessToken = process.env.Content_Delivery_API_access_token
+
+const client = require('contentful').createClient({
+  space: space,
+  accessToken: accessToken,
+})
+
+export async function fetchEntries() {
+  const entries = await client.getEntries()
+  if (entries.items) return entries.items
+  console.log(`Error getting Entries for ${contentType.name}.`)
+}
+
+export default { fetchEntries }
