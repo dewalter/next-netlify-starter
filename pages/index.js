d7fcf715a487906532fca06ef25df77ce0b001fe
Walter <wkrijt@gmail.com>
2021-03-08 15:55:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { fetchEntries } from '@contentfulPosts'
-import Post from '@components/Post'
 
-// inside your component markup, pull `posts` from props
-
-<div className="posts">
-  {posts.map((p) => {
-    return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-  })}
-</div>
-
-// at the bottom of your component file
-
-export async function getStaticProps() {
-  const res = await fetchEntries()
-  const posts = await res.map((p) => {
-    return p.fields
-  })
-
-  return {
-    props: {
-      posts,
-    },
-  }
-}
 
 export default function Home() {
   return (
